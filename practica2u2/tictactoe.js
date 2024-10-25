6d475ffc209392d4c86b3800c69f4d1f168124fb
xmoren025 <2130293@upv.edu.mx>
2024-10-24 18:12:48 -0600

    jsUpdate

--- practica2u2/tictactoe.js
+++ practica2u2/tictactoe.js
@@ -1,6 +1,6 @@
-const selectBox = document.querySelector(".select-box");
-const selectBtnM = selectBox.querySelector(".options.player1");
-const selectBtnN = selectBox.querySelector(".options.player2");
+const selectBox = document.querySelector(".team-selector"); 
+const selectBtnM = selectBox.querySelector(".player1");
+const selectBtnN = selectBox.querySelector(".player2");
 const playBoard = document.querySelector(".table");
 const players = document.querySelector(".players");
 const allBox = document.querySelectorAll("section span");
@@ -8,18 +8,18 @@ const resultBox = document.querySelector(".result-box");
 const wonText = resultBox.querySelector(".won-text");
 const replayBtn = resultBox.querySelector("button");
 
-window.onload = ()=>{
-    for (let i = 0; i < allBox-length; i++){
+window.onload = () => {
+    for (let i = 0; i < allBox.length; i++) {
         allBox[i].setAttribute("onclick", "clickedBox(this)");
     }
 }
 
-selectBtnM.onclick = ()=>{
+selectBtnM.onclick = () => {
     selectBox.classList.add("hide");
     playBoard.classList.add("show");
 }
 
-selectBtnN.onclick =()=>{
+selectBtnN.onclick = () => {
     selectBox.classList.add("hide");
     playBoard.classList.add("show");
     players.setAttribute("class", "players active player");
@@ -27,88 +27,91 @@ selectBtnN.onclick =()=>{
 
 let player1Icon = "./img/maki.png";
 let player2Icon = "./img/nigiri.png";
-let playerSign = "M"
+let playerSign = "M";
 let runBot = true;
 
-function clickedBox(element){
-    if(players.classList.contains("player")){
+function clickedBox(element) {
+    if (players.classList.contains("player")) {
         playerSign = "N";
-        element.innerHTML = `<i class="${player2Icon}"></i>`;
+        element.innerHTML = `<img src="${player2Icon}" alt="Nigiri" style="width: 100%;">`;
         players.classList.remove("active");
         element.setAttribute("id", playerSign);
-    }else{
-        element.innerHTML = `<i class="${player1Icon}"></i>`;
-        element.setAttribute("id",playerSign);
+    } else {
+        element.innerHTML = `<img src="${player1Icon}" alt="Maki" style="width: 100%;">`;
+        element.setAttribute("id", playerSign);
         players.classList.add("active");
     }
     selectWinner();
     element.style.pointerEvents = "none";
     playBoard.style.pointerEvents = "none";
+
     let randomTimeDelay = ((Math.random() * 1000) + 200).toFixed();
-    setTimeout(()=>{
+    setTimeout(() => {
         bot(runBot);
     }, randomTimeDelay);
 }
 
-function bot(){
+function bot() {
     let array = [];
-    if(runBot){
+    if (runBot) {
         playerSign = "N";
-        for (let i = 0; i < allBox.length; i++){
-            if(allBox[i].childElementCount== 0){
+        for (let i = 0; i < allBox.length; i++) {
+            if (allBox[i].childElementCount == 0) {
                 array.push(i);
-            }    
+            }
         }
-        let randomBox =array[Math.floor(Math.random() * array.length)];
-        if(array.length> 0){
-            if(players.classList.classList.contains("player")){
+        let randomBox = array[Math.floor(Math.random() * array.length)];
+        if (array.length > 0) {
+            if (players.classList.contains("player")) {
                 playerSign = "M";
-                allBox[randomBox].innerHTML = `<i class="${player1Icon}"></i>`;
+                allBox[randomBox].innerHTML = `<img src="${player1Icon}" alt="Maki" style="width: 100%;">`;
                 allBox[randomBox].setAttribute("id", playerSign);
                 players.classList.add("active");
-            }else{
-                allBox[randomBox].innerHTML = `<i class="${player2Icon}"></i>`;
+            } else {
+                allBox[randomBox].innerHTML = `<img src="${player2Icon}" alt="Nigiri" style="width: 100%;">`;
                 players.classList.remove("active");
                 allBox[randomBox].setAttribute("id", playerSign);
             }
             selectWinner();
         }
         allBox[randomBox].style.pointerEvents = "none";
-        playBoard.style.pointerEvents= "auto";
+        playBoard.style.pointerEvents = "auto";
         playerSign = "M";
     }
 }
 
-function getIdVal(classname){
+function getIdVal(classname) {
     return document.querySelector(".box" + classname).id;
 }
-function checkIdSign(val1, val2,val3, sign){
-    if(getIdVal(val1) == sign && getIdVal(val2) == sign && getIdVal(val3) == sign){
+
+function checkIdSign(val1, val2, val3, sign) {
+    if (getIdVal(val1) == sign && getIdVal(val2) == sign && getIdVal(val3) == sign) {
         return true;
     }
 }
-function selectWinner(){
-    if(checkIdSign(1,2,3,playerSign) || checkIdSign(4,5,6,playerSign) || checkIdSign(7,8,9, playerSign) || checkIdSign(1,4,7,playerSign) || checkIdSign(2,5,8, playerSign) || checkIdSign(3,6,9,playerSign) || checkIdSign(1,5,9,playerSign) || checkIdSign(3,5,7,playerSign)){
+
+function selectWinner() {
+    if (checkIdSign(1, 2, 3, playerSign) || checkIdSign(4, 5, 6, playerSign) || checkIdSign(7, 8, 9, playerSign) || checkIdSign(1, 4, 7, playerSign) || checkIdSign(2, 5, 8, playerSign) || checkIdSign(3, 6, 9, playerSign) || checkIdSign(1, 5, 9, playerSign) || checkIdSign(3, 5, 7, playerSign)) {
         runBot = false;
         bot(runBot);
-        setTimeout(()=>{
+        setTimeout(() => {
             resultBox.classList.add("show");
             playBoard.classList.remove("show");
         }, 700);
-        wonText.innerHTML =`Player <p>${playerSign}</p> won the game!`;
-    }else{
-        if(getIdVal(1) != "" && getIdVal(2) != "" && getIdVal(3) != "" && getIdVal(4) != "" && getIdVal(5) != "" && getIdVal(6) != "" && getIdVal(7) != "" && getIdVal(8) != "" && getIdVal(9) != ""){
+        wonText.innerHTML = `Player <p>${playerSign}</p> won the game!`;
+    } else {
+        if (getIdVal(1) != "" && getIdVal(2) != "" && getIdVal(3) != "" && getIdVal(4) != "" && getIdVal(5) != "" && getIdVal(6) != "" && getIdVal(7) != "" && getIdVal(8) != "" && getIdVal(9) != "") {
             runBot = false;
             bot(runBot);
-            setTimeout(()=>{
+            setTimeout(() => {
                 resultBox.classList.add("show");
                 playBoard.classList.remove("show");
-            },700);
+            }, 700);
             wonText.textContent = "Match has been drawn!";
         }
     }
 }
 
-replayBtn.onclick = ()=>{
-    window.location.reload;
+replayBtn.onclick = () => {
+    window.location.reload();
 }
